7a45650e66c130c92f02c24f4b3e4f2a636e1069
Solterobenja <be.montecinosp@duocuc.cl>
2025-09-08 23:26:12 -0300

    inicio ahora si

---
+++ miBarrioDigital/src/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export function Home(){
+    return(
+        <>
+        <body>
+            <h1>Hola</h1>
+        </body>
+        <h1>adios</h1>
+        </>
+    )
+}
