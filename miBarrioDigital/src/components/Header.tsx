33856f3d1a3cbbb3d7e25beab38c72ae4e70bc6b
leon2023mino <le.mino@duocuc.cl>
2025-10-01 14:59:17 -0300

    titulo nuevo

--- miBarrioDigital/src/components/Header.tsx
+++ miBarrioDigital/src/components/Header.tsx
@@ -1,47 +1,39 @@
 import "../App.css";
-import logo from "../assets/logopng.png";
+import titulo from "../assets/logo_mibarrio.png";
 import { NavLink } from "react-router-dom";
 
 export function Header() {
   return (
     <div>
       {/* Header superior */}
-      <header className="header-top">
-        {/* Izquierda: logo + título */}
-        <div className="logo">
-          <img src={logo} alt="Logo" />
-          <h1>Mi Barrio Digital</h1>
+      <header className="header-top" role="banner">
+        {/* Izquierda: solo el título como imagen */}
+        <div className="logo" aria-label="Mi Barrio Digital">
+          <img src={titulo} alt="Mi Barrio Digital" className="titulo-img" />
         </div>
 
-        {/* Derecha: botón de registro */}
-        <div className="header-actions">
-          <NavLink to="/Login" className="btn-registro">
-            Ingreso
+        {/* Derecha: contacto (solo texto) + botón de login */}
+        <div className="header-actions" aria-hidden={false}>
+          <div className="header-contact">
+            <span className="contact-text">contacto@mibarriodigital.cl</span>
+            <span className="contact-text">+56 9 1234 5678</span>
+          </div>
+
+          <NavLink to="/Login" className="btn-registro" aria-label="Iniciar sesión">
+            Iniciar Sesión
           </NavLink>
         </div>
       </header>
 
       {/* Header inferior con navegación */}
-      <header className="header-bottom">
+      <header className="header-bottom" role="navigation" aria-label="Menú principal">
         <nav>
-          <NavLink to="/" end className="nav-link">
-            INICIO
-          </NavLink>
-          <NavLink to="/certificados" className="nav-link">
-            CERTIFICADO
-          </NavLink>
-          <NavLink to="/proyectos" className="nav-link">
-            PROYECTOS
-          </NavLink>
-          <NavLink to="/reservas" className="nav-link">
-            RESERVAS
-          </NavLink>
-          <NavLink to="/noticias" className="nav-link">
-            NOTICIAS
-          </NavLink>
-          <NavLink to="/contacto" className="nav-link">
-            CONTACTO
-          </NavLink>
+          <NavLink to="/" end className="nav-link">INICIO</NavLink>
+          <NavLink to="/certificados" className="nav-link">CERTIFICADO</NavLink>
+          <NavLink to="/proyectos" className="nav-link">PROYECTOS</NavLink>
+          <NavLink to="/reservas" className="nav-link">RESERVAS</NavLink>
+          <NavLink to="/noticias" className="nav-link">NOTICIAS</NavLink>
+          <NavLink to="/contacto" className="nav-link">CONTACTO</NavLink>
         </nav>
       </header>
     </div>
